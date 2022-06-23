6f36e837dd04660f3808c1ce54ede92cdc153daa
ionian-dev <ionian-dev@hotmail.com>
2022-06-23 16:22:27 +0800

    IonianLog v2

--- src/IonianLog.sol
+++ src/IonianLog.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity =0.8.15;
 
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 interface AccessControl {
     // do not use `view` so that we allow some bookkeeping
@@ -9,16 +9,16 @@ interface AccessControl {
 }
 
 interface IonianStructs {
+    // TODO: optimize this with tight variable packing
     struct LogEntry {
         uint256[] streamIds;
-        string data;
+        bytes data;
         bytes32 dataRoot;
+        uint256 numChunks;
     }
 
     struct Stream {
-        uint256 id;
         AccessControl ac;
-        uint256[] indices;
     }
 }
 
@@ -26,173 +26,100 @@ interface IonianEvents {
     event NewStream(uint256 id);
 }
 
-contract IonianLog is IonianStructs, IonianEvents, ReentrancyGuard {
+contract IonianLog is IonianStructs, IonianEvents {
     // ------------------ constants ------------------
 
-    uint256 public constant DEFAULT_STREAM = 0;
     uint256 public constant MAX_STREAMS_PER_LOG = 10;
-    uint256 public constant UINT_MAX = 2**256 - 1;
-    uint256 public constant MAX_DATA_LENGTH = 200;
 
     // ------------------ state variables ------------------
 
     LogEntry[] public log;
-    uint256 public nextStreamId = 1;
-    mapping(uint256 => Stream) public streams;
+    Stream[] public streams;
+
+    // ------------------ initialization ------------------
+
+    constructor() {
+        // reserve stream #0
+        streams.push(Stream(AccessControl(address(0))));
+    }
 
     // ------------------ log management ------------------
 
-    function appendLog(string calldata data, bytes32 dataRoot)
-        external
-        nonReentrant
-    {
-        require(bytes(data).length <= MAX_DATA_LENGTH, "Error");
+    function appendLog(
+        bytes calldata data,
+        bytes32 dataRoot,
+        uint256 numChunks
+    ) external {
+        require(
+            data.length == 0 || dataRoot == bytes32(0),
+            "Must specify one of data and dataRoot"
+        );
 
-        uint256[] memory streamIds = new uint256[](1);
-        streamIds[0] = DEFAULT_STREAM;
-        log.push(LogEntry(streamIds, data, dataRoot));
+        LogEntry memory entry;
+        entry.data = data;
+        entry.dataRoot = dataRoot;
+        entry.numChunks = numChunks;
+
+        log.push(entry);
     }
 
     function appendLog(
         uint256[] calldata streamIds,
-        string calldata data,
-        bytes32 dataRoot
-    ) external nonReentrant {
-        require(streamIds.length <= MAX_STREAMS_PER_LOG, "Error");
-        require(bytes(data).length <= MAX_DATA_LENGTH, "Error");
+        bytes calldata data,
+        bytes32 dataRoot,
+        uint256 numChunks
+    ) external {
+        require(
+            data.length == 0 || dataRoot == bytes32(0),
+            "Must specify one of data and dataRoot"
+        );
 
-        uint256 logId = log.length;
-        log.push(LogEntry(streamIds, data, dataRoot));
+        log.push(LogEntry(streamIds, data, dataRoot, numChunks));
+
+        require(streamIds.length <= MAX_STREAMS_PER_LOG, "Error");
 
         for (uint256 ii = 0; ii < streamIds.length; ii++) {
             uint256 streamId = streamIds[ii];
-            Stream storage stream = streams[streamId];
+            Stream memory stream = streams[streamId];
 
             if (stream.ac != AccessControl(address(0))) {
                 require(stream.ac.canAppend(msg.sender), "Unauthorized");
             }
-
-            stream.indices.push(logId);
         }
     }
 
     // ------------------ stream management ------------------
 
     function createStream(AccessControl ac) external {
-        uint256 id = nextStreamId++;
-
-        streams[id].id = id;
-        streams[id].ac = ac;
-
+        uint256 id = streams.length;
+        streams.push(Stream(ac));
         emit NewStream(id);
     }
 
     // ------------------ query interface ------------------
 
-    function belongsTo(uint256 logId, uint256[] memory streamIds)
-        public
-        view
-        returns (bool)
-    {
-        for (uint256 ii = 0; ii < streamIds.length; ++ii) {
-            for (uint256 jj = 0; jj < log[logId].streamIds.length; ++jj) {
-                if (streamIds[ii] == log[logId].streamIds[jj]) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+    function numLogs() external view returns (uint256) {
+        return log.length;
     }
 
-    function iterateStreams(
-        uint256[] calldata streamIds,
-        uint256 fromLogId,
-        uint256 maxToReturn
-    )
-        external
-        view
-        returns (
-            LogEntry[] memory result,
-            uint256 num,
-            uint256 next
-        )
-    {
-        require(streamIds.length <= MAX_STREAMS_PER_LOG, "Error");
-
-        result = new LogEntry[](maxToReturn);
-        next = fromLogId;
-
-        while (next < log.length && num < maxToReturn) {
-            uint256 logId = next++;
-
-            if (belongsTo(logId, streamIds)) {
-                result[num++] = log[logId];
-            }
-        }
+    function numStreams() external view returns (uint256) {
+        return streams.length;
     }
 
-    function iterateStreams2(
-        uint256[] calldata streamIds,
-        uint256[] calldata from,
-        uint256 maxToReturn
-    )
+    function getLogs(uint256 offset, uint256 limit)
         external
         view
-        returns (
-            LogEntry[] memory result,
-            uint256 num,
-            uint256[] memory next
-        )
+        returns (LogEntry[] memory entries)
     {
-        require(streamIds.length <= MAX_STREAMS_PER_LOG, "Error");
-        require(streamIds.length == from.length, "Error");
+        if (offset >= log.length) {
+            return new LogEntry[](0);
+        }
 
-        result = new LogEntry[](maxToReturn);
-        next = from;
+        uint256 endExclusive = Math.min(log.length, offset + limit);
+        entries = new LogEntry[](endExclusive - offset);
 
-        while (num < maxToReturn) {
-            uint256 minLogId = UINT_MAX;
-
-            // check next log id for streams, select the smallest
-            for (uint256 ii = 0; ii < streamIds.length; ++ii) {
-                uint256 streamId = streamIds[ii];
-                uint256 nextId = next[ii];
-
-                if (nextId >= streams[streamId].indices.length) {
-                    continue;
-                }
-
-                uint256 logId = streams[streamId].indices[nextId];
-
-                if (logId < minLogId) {
-                    minLogId = logId;
-                }
-            }
-
-            // all streams are exhausted
-            if (minLogId == UINT_MAX) {
-                return (result, num, next);
-            }
-
-            // collect result
-            result[num++] = log[minLogId];
-
-            // update next ids
-            for (uint256 ii = 0; ii < streamIds.length; ++ii) {
-                uint256 streamId = streamIds[ii];
-                uint256 nextId = next[ii];
-
-                if (nextId >= streams[streamId].indices.length) {
-                    continue;
-                }
-
-                uint256 logId = streams[streamId].indices[nextId];
-
-                if (logId == minLogId) {
-                    next[ii] += 1;
-                }
-            }
+        for (uint256 ii = offset; ii < endExclusive; ++ii) {
+            entries[ii - offset] = log[ii];
         }
     }
 }
--- src/test/IonianLog.t.sol
+++ src/test/IonianLog.t.sol
@@ -75,9 +75,13 @@ contract SimpleAppend is BaseSetup {
 
         vm.expectEmit(true, true, true, true, address(ionian));
         emit NewStream(1);
-
         ionian.createStream(AccessControl(address(0)));
-        ionian.appendLog(stream1only, "data1", bytes32("root1"));
+
+        ionian.appendLog(stream1only, "", bytes32("root1"), 32);
+        ionian.appendLog(stream1only, "data1", bytes32(""), 32);
+
+        vm.expectRevert("Must specify one of data and dataRoot");
+        ionian.appendLog(stream1only, "data1", bytes32("root1"), 32);
 
         vm.stopPrank();
     }
@@ -92,20 +96,20 @@ contract AppendWithAccessControl is BaseSetup {
         // new stream by Alice
         vm.startPrank(alice);
         ionian.createStream(AccessControl(address(0)));
-        ionian.appendLog(stream1only, "data1", bytes32("root1"));
+        ionian.appendLog(stream1only, "", bytes32("root1"), 32);
         vm.stopPrank();
 
         // new stream by Bob
         vm.startPrank(bob);
         SimpleAccessControl ac = new SimpleAccessControl();
         ionian.createStream(ac);
-        ionian.appendLog(stream2only, "data2", bytes32("root2"));
+        ionian.appendLog(stream2only, "", bytes32("root2"), 32);
         vm.stopPrank();
 
         // access denied
         vm.startPrank(alice);
         vm.expectRevert("Unauthorized");
-        ionian.appendLog(streams1and2, "data3", bytes32("root3"));
+        ionian.appendLog(streams1and2, "", bytes32("root3"), 32);
         vm.stopPrank();
 
         // grant access
@@ -113,228 +117,6 @@ contract AppendWithAccessControl is BaseSetup {
         ac.approve(alice);
 
         vm.prank(alice);
-        ionian.appendLog(streams1and2, "data3", bytes32("root3"));
-    }
-}
-
-contract ShortStreamQueries is BaseSetup {
-    uint256 public constant MAX = 5;
-
-    function setUp() public virtual override {
-        BaseSetup.setUp();
-
-        // create two streams
-        vm.startPrank(alice);
-        ionian.createStream(AccessControl(address(0)));
-        ionian.createStream(AccessControl(address(0)));
-
-        // create log
-        ionian.appendLog(stream1only, "data1", bytes32("root1"));
-        ionian.appendLog(stream2only, "data2", bytes32("root2"));
-        ionian.appendLog(streams1and2, "data3", bytes32("root3"));
-        vm.stopPrank();
-    }
-
-    function testFewLogs1() public {
-        LogEntry[] memory entries;
-        uint256 num;
-        uint256 next;
-
-        // stream #1
-        (entries, num, next) = ionian.iterateStreams(stream1only, 0, MAX);
-
-        assertEq(num, 2);
-        assertEq(next, 3);
-        assertEq(entries[0].dataRoot, bytes32("root1"));
-        assertEq(entries[1].dataRoot, bytes32("root3"));
-
-        // stream #2
-        (entries, num, next) = ionian.iterateStreams(stream2only, 0, MAX);
-
-        assertEq(num, 2);
-        assertEq(next, 3);
-        assertEq(entries[0].dataRoot, bytes32("root2"));
-        assertEq(entries[1].dataRoot, bytes32("root3"));
-
-        // streams #1 and #2
-        (entries, num, next) = ionian.iterateStreams(streams1and2, 0, MAX);
-
-        assertEq(num, 3);
-        assertEq(next, 3);
-        assertEq(entries[0].dataRoot, bytes32("root1"));
-        assertEq(entries[1].dataRoot, bytes32("root2"));
-        assertEq(entries[2].dataRoot, bytes32("root3"));
-    }
-
-    function testFewLogs2() public {
-        LogEntry[] memory entries;
-        uint256 num;
-        uint256[] memory next;
-
-        // stream #1
-        uint256[] memory from = new uint256[](1);
-        from[0] = 0;
-
-        (entries, num, next) = ionian.iterateStreams2(stream1only, from, MAX);
-
-        assertEq(num, 2);
-        assertEq(next[0], 2);
-        assertEq(entries[0].dataRoot, bytes32("root1"));
-        assertEq(entries[1].dataRoot, bytes32("root3"));
-
-        // stream #2
-        (entries, num, next) = ionian.iterateStreams2(stream2only, from, MAX);
-
-        assertEq(num, 2);
-        assertEq(next[0], 2);
-        assertEq(entries[0].dataRoot, bytes32("root2"));
-        assertEq(entries[1].dataRoot, bytes32("root3"));
-
-        // streams #1 and #2
-        uint256[] memory from2 = new uint256[](2);
-        from2[0] = 0;
-        from2[1] = 0;
-
-        (entries, num, next) = ionian.iterateStreams2(streams1and2, from2, MAX);
-
-        assertEq(num, 3);
-        assertEq(next[0], 2);
-        assertEq(next[1], 2);
-        assertEq(entries[0].dataRoot, bytes32("root1"));
-        assertEq(entries[1].dataRoot, bytes32("root2"));
-        assertEq(entries[2].dataRoot, bytes32("root3"));
-    }
-}
-
-contract LongStreamQueries is BaseSetup {
-    uint256 public constant MAX = 1000;
-
-    uint256[] internal from;
-    uint256[] internal from2;
-
-    function setUp() public virtual override {
-        BaseSetup.setUp();
-
-        // create two streams
-        vm.startPrank(alice);
-        ionian.createStream(AccessControl(address(0)));
-        ionian.createStream(AccessControl(address(0)));
-        vm.stopPrank();
-
-        // utilities
-        from = new uint256[](1);
-        from[0] = 0;
-
-        from2 = new uint256[](2);
-        from2[0] = 0;
-        from2[1] = 0;
-    }
-}
-
-contract DenseStreamQueries is LongStreamQueries {
-    function setUp() public virtual override {
-        LongStreamQueries.setUp();
-
-        // create log
-        vm.startPrank(alice);
-
-        for (uint256 ii = 0; ii < MAX; ++ii) {
-            if (ii % 3 == 0) {
-                ionian.appendLog(stream1only, "one", bytes32("one"));
-            } else if (ii % 3 == 1) {
-                ionian.appendLog(stream2only, "two", bytes32("two"));
-            } else {
-                ionian.appendLog(streams1and2, "both", bytes32("both"));
-            }
-        }
-
-        vm.stopPrank();
-    }
-
-    function testDenseLogs1() public {
-        uint256 num;
-
-        // stream #1
-        (, num, ) = ionian.iterateStreams(stream1only, 0, MAX);
-        assertEq(num, 667);
-
-        // stream #2
-        (, num, ) = ionian.iterateStreams(stream2only, 0, MAX);
-        assertEq(num, 666);
-
-        // stream2 #1 and #2
-        (, num, ) = ionian.iterateStreams(streams1and2, 0, MAX);
-        assertEq(num, MAX);
-    }
-
-    function testDenseLogs2() public {
-        uint256 num;
-
-        // stream #1
-        (, num, ) = ionian.iterateStreams2(stream1only, from, MAX);
-        assertEq(num, 667);
-
-        // stream #2
-        (, num, ) = ionian.iterateStreams2(stream2only, from, MAX);
-        assertEq(num, 666);
-
-        // streams #1 and #2
-        (, num, ) = ionian.iterateStreams2(streams1and2, from2, MAX);
-        assertEq(num, MAX);
-    }
-}
-
-contract SparseStreamQueries is LongStreamQueries {
-    function setUp() public virtual override {
-        LongStreamQueries.setUp();
-
-        // create log
-        vm.startPrank(alice);
-
-        for (uint256 ii = 0; ii < MAX; ++ii) {
-            if (ii % 300 == 0) {
-                ionian.appendLog(streams1and2, "both", bytes32("both"));
-            } else if (ii % 100 == 0) {
-                ionian.appendLog(stream1only, "one", bytes32("one"));
-            } else if (ii % 150 == 0) {
-                ionian.appendLog(stream2only, "two", bytes32("two"));
-            } else {
-                ionian.appendLog("null", bytes32("null"));
-            }
-        }
-
-        vm.stopPrank();
-    }
-
-    function testSparseLogs1() public {
-        uint256 num;
-
-        // stream #1
-        (, num, ) = ionian.iterateStreams(stream1only, 0, MAX);
-        assertEq(num, 10);
-
-        // stream #2
-        (, num, ) = ionian.iterateStreams(stream2only, 0, MAX);
-        assertEq(num, 7);
-
-        // stream2 #1 and #2
-        (, num, ) = ionian.iterateStreams(streams1and2, 0, MAX);
-        assertEq(num, 13);
-    }
-
-    function testSparseLogs2() public {
-        uint256 num;
-
-        // stream #1
-        (, num, ) = ionian.iterateStreams2(stream1only, from, MAX);
-        assertEq(num, 10);
-
-        // stream #2
-        (, num, ) = ionian.iterateStreams2(stream2only, from, MAX);
-        assertEq(num, 7);
-
-        // streams #1 and #2
-        (, num, ) = ionian.iterateStreams2(streams1and2, from2, MAX);
-        assertEq(num, 13);
+        ionian.appendLog(streams1and2, "", bytes32("root3"), 32);
     }
 }
